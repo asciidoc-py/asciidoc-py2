71fc5096a4a0923b93d2d15504e17195af199e9f
Stuart Rackham <srackham@methods.co.nz>
2009-03-16 12:47:19 +1300

    Dropped inline inputs and outputs in conf file after using them:
    - Cumbersome in that inline outputs overwhelm the conf file.
    - Separate input files are easier to edit and have syntax highlighting.
    - The conf file doesn't have to be update by update command.
    - Inline stuff adds hugely to program complexity.
    - Eliminates inline input/output being mistaken for a directive.

--- tests/testasciidoc.py
+++ tests/testasciidoc.py
@@ -57,13 +57,10 @@ class AsciiDocTest(object):
         self.number = None      # Test number (1..).
         self.title = ''         # Optional title.
         self.description = []   # List of lines followoing title.
+        self.filename = None    # AsciiDoc test source file.
         self.options = []
         self.attributes = {}
         self.backends = BACKENDS
-        self.asciidoc = None    # AsciiDoc input (list of strings or file name).
-        self.inline = True  # True if asciidoc input is not in external file.
-        self.header = []    # List of lines from conf file test (excludes inline outputs).
-        self.expected = {}      # Expected inline outputs keyed by backend.
         self.datadir = None     # Where output files are stored.
 
     def backend_filename(self, backend):
@@ -71,11 +68,10 @@ class AsciiDocTest(object):
         Return the path name of the backend  output file that is generated from
         the AsciiDoc sourcefile.
         """
-        assert not self.inline
         return '%s-%s%s' % (
                 os.path.normpath(
                     os.path.join(self.datadir,
-                        os.path.basename(os.path.splitext(self.asciidoc)[0]))),
+                        os.path.basename(os.path.splitext(self.filename)[0]))),
                 backend,
                 BACKEND_EXT[backend])
 
@@ -87,12 +83,10 @@ class AsciiDocTest(object):
         self.confdir = confdir
         self.datadir = datadir
         lines = Lines(lines)
-        self.header = []
         while not lines.eol():
             l = lines.read_until(r'^%')
             if l:
                 if not l[0].startswith('%'):
-                    self.header += l
                     self.description = l
                     self.title = l[0]
                     continue
@@ -102,64 +96,40 @@ class AsciiDocTest(object):
                 directive = reo.groupdict()['directive']
                 data = normalize_data(l[1:])
                 if directive == 'asciidoc':
-                    self.header += l
-                    reo = re.match(r'%\s*asciidoc(:\s*(?P<fname>.*?)\s*)?$', l[0])
-                    fname = reo.groupdict()['fname']
-                    if fname:
-                        self.inline = False
-                        self.asciidoc = os.path.normpath(os.path.join(
-                                self.confdir, os.path.normpath(fname)))
-                    else:
-                        self.inline = True
-                        self.asciidoc = data
+                    self.filename = os.path.normpath(os.path.join(
+                            self.confdir, os.path.normpath(l[1])))
                 elif directive == 'options':
-                    self.header += l
                     self.options = eval(' '.join(data))
                     for i,v in enumerate(self.options):
                         if isinstance(v, basestring):
                             self.options[i] = (v,None)
                 elif directive == 'attributes':
-                    self.header += l
                     self.attributes = eval(' '.join(data))
                 elif directive == 'backends':
-                    self.header += l
                     self.backends = eval(' '.join(data))
-                elif directive in BACKENDS:
-                    assert self.inline
-                    self.expected[directive] = data
                 else:
-                    raise Exception
+                    raise (ValueError,
+                           'illegal configuration directive: %s' % directive)
         if not self.title:
-            if self.inline:
-                self.title = self.asciidoc[0]
-            else:
-                self.title = self.asciidoc
-        if self.inline and not self.options:
-            self.options = [('--no-header-footer',None)]
+            self.title = self.filename
 
     def has_expected(self, backend):
         """
         Returns True if there is output test data for backend.
         """
-        if self.inline:
-            return  self.expected.has_key(backend)
-        else:
-            return os.path.isfile(self.backend_filename(backend))
+        return os.path.isfile(self.backend_filename(backend))
 
     def get_expected(self, backend):
         """
         Return expected test data output for backend.
         """
-        if self.inline:
-            result = self.expected.get(backend)
-        else:
-            f = open(self.backend_filename(backend))
-            try:
-                result = f.readlines()
-                # Strip line terminators.
-                result = [ s.rstrip() for s in result ]
-            finally:
-                f.close()
+        f = open(self.backend_filename(backend))
+        try:
+            result = f.readlines()
+            # Strip line terminators.
+            result = [ s.rstrip() for s in result ]
+        finally:
+            f.close()
         return result
 
     def generate_expected(self, backend):
@@ -169,10 +139,7 @@ class AsciiDocTest(object):
         asciidoc = asciidocapi.AsciiDocAPI()
         asciidoc.options.values = self.options
         asciidoc.attributes = self.attributes
-        if self.inline:
-            infile = StringIO.StringIO(os.linesep.join(self.asciidoc))
-        else:
-            infile = self.asciidoc
+        infile = self.filename
         outfile = StringIO.StringIO()
         asciidoc.execute(infile, outfile, backend)
         return outfile.getvalue().splitlines()
@@ -182,35 +149,19 @@ class AsciiDocTest(object):
         Generate and write backend data.
         """
         lines = self.generate_expected(backend)
-        if self.inline:
-            self.expected[backend] = lines
-        else:
-            if not os.path.isdir(self.datadir):
-                message('CREATING: %s' % self.datadir)
-                os.mkdir(self.datadir)
-            f = open(self.backend_filename(backend),'w+')
-            try:
-                message('WRITING: %s' % f.name)
-                f.writelines([ s + os.linesep for s in lines])
-            finally:
-                f.close()
-
-    def spec(self):
-        """
-        Return test specification.
-        """
-        result = self.header
-        if self.inline:
-            for backend in self.expected.keys():
-                result.append('%% %s' % backend)
-                result.extend(self.expected[backend])
-                result.append('')
-        return result
+        if not os.path.isdir(self.datadir):
+            message('CREATING: %s' % self.datadir)
+            os.mkdir(self.datadir)
+        f = open(self.backend_filename(backend),'w+')
+        try:
+            message('WRITING: %s' % f.name)
+            f.writelines([ s + os.linesep for s in lines])
+        finally:
+            f.close()
 
     def update(self, backend=None, force=False):
         """
         Regenerate and update expected test data outputs.
-        Returns list of strings containing test configuration file section.
         """
         if backend is None:
             backends = self.backends
@@ -219,7 +170,6 @@ class AsciiDocTest(object):
         for backend in backends:
             if force or not self.has_expected(backend):
                 self.update_expected(backend)
-        return self.spec()
 
     def run(self, backend=None):
         """
@@ -229,18 +179,16 @@ class AsciiDocTest(object):
         result = True   # Assume success.
         self.passed = self.failed = self.skipped = 0
         message('%d: %s' % (self.number, self.title))
-        if not self.inline:
-            if os.path.isfile(self.asciidoc):
-                message(self.asciidoc)
-            else:
-                message('MISSING: %s' % self.asciidoc)
+        if os.path.isfile(self.filename):
+            message(self.filename)
+        else:
+            message('MISSING: %s' % self.filename)
         if backend is None:
             backends = self.backends
         else:
             backends = [backend]
         for backend in backends:
-            if not self.inline:
-                fromfile = self.backend_filename(backend)
+            fromfile = self.backend_filename(backend)
             if self.has_expected(backend):
                 expected = self.get_expected(backend)
                 strip_end(expected)
@@ -254,10 +202,7 @@ class AsciiDocTest(object):
                     self.failed +=1
                     lines = lines[3:]
                     message('FAILED: %s' % backend)
-                    if self.inline:
-                        message('+++ expected')
-                    else:
-                        message('+++ %s' % fromfile)
+                    message('+++ %s' % fromfile)
                     message('--- got')
                     for line in lines:
                         message(line)
@@ -284,7 +229,6 @@ class AsciiDocTests(object):
         self.datadir = self.confdir # Default expected files directory.
         self.tests = []             # List of parsed AsciiDocTest objects.
         self.globals = {}
-        self.header = []    # %globals lines.
         f = open(self.conffile)
         try:
             lines = Lines(f.readlines())
@@ -296,7 +240,6 @@ class AsciiDocTests(object):
             if s:
                 # Optional globals precede all tests.
                 if first and re.match(r'^%\s*globals$',s[0]):
-                    self.header = s
                     self.globals = eval(' '.join(normalize_data(s[1:])))
                     if 'datadir' in self.globals:
                         self.datadir = os.path.join(
@@ -332,20 +275,9 @@ class AsciiDocTests(object):
         """
         Regenerate expected test data and update configuratio file.
         """
-        result = self.header
         for test in self.tests:
-            result.append('%' * 72)
             if not number or number == test.number:
-                result.extend(test.update(backend, force=force))
-            else:
-                result.extend(test.spec())
-        os.rename(self.conffile, self.conffile + '.orig')
-        message('WRITING: %s' % self.conffile)
-        f = open(self.conffile, 'w')
-        try:
-            f.write(os.linesep.join(result))
-        finally:
-            f.close()
+                test.update(backend, force=force)
 
     def list(self):
         """

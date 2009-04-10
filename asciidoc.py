1f2ccea41ffa3c3eea7ca0c1140c705bfd4544df
Stuart Rackham <srackham@methods.co.nz>
2009-04-10 18:17:47 +1200

    Bumped version number and created CHANGELOG entry.

--- asciidoc.py
+++ asciidoc.py
@@ -9,7 +9,7 @@ under the terms of the GNU General Public License (GPL).
 import sys, os, re, time, traceback, tempfile, subprocess, codecs, locale
 
 ### Used by asciidocapi.py ###
-VERSION = '8.4.2'   # See CHANGLOG file for version history.
+VERSION = '8.4.3'   # See CHANGLOG file for version history.
 
 MIN_PYTHON_VERSION = 2.4   # Require this version of Python or better.
 

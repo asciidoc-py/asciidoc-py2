b95987ff463b9ec7a0097bb6cf528926cc93b0b6
Stuart Rackham <srackham@methods.co.nz>
2010-08-03 14:33:26 +1200

    Dropped deprecated --unsafe asciidoc attribute option in examples/website/build-website.sh script.

--- examples/website/build-website.sh
+++ examples/website/build-website.sh
@@ -4,10 +4,10 @@ VERS="8.1.0"
 DATE="2006-10-22"
 
 # Leave the desired layout uncommented.
-LAYOUT=layout1		# Tables based layout.
-#LAYOUT=layout2		# CSS based simulated frames layout.
+LAYOUT=layout1      # Tables based layout.
+#LAYOUT=layout2     # CSS based simulated frames layout.
 
-ASCIIDOC_HTML="python ../../asciidoc.py --unsafe --backend=xhtml11 --conf-file=${LAYOUT}.conf --attribute icons --attribute iconsdir=./images/icons --attribute=badges --attribute=revision=$VERS  --attribute=date=$DATE"
+ASCIIDOC_HTML="python ../../asciidoc.py --backend=xhtml11 --conf-file=${LAYOUT}.conf --attribute icons --attribute iconsdir=./images/icons --attribute=badges --attribute=revision=$VERS  --attribute=date=$DATE"
 
 $ASCIIDOC_HTML -a index-only index.txt
 $ASCIIDOC_HTML -a toc -a numbered userguide.txt

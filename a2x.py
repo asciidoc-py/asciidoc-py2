c628b432ebb18e7e8f6b5ac07e554b5655a1ad96
jvdh <vandenhoff.j@googlemail.com>
2014-05-08 12:56:08 +0200

    Include `-dump' into W3M_OPTS, LYNX_OPTS. adjusted `a2x' manpage to document the changes (also fixed a few typos).

--- a2x.py
+++ a2x.py
@@ -45,8 +45,8 @@ ASCIIDOC = 'asciidoc'
 XSLTPROC = 'xsltproc'
 DBLATEX = 'dblatex'         # pdf generation.
 FOP = 'fop'                 # pdf generation (--fop option).
-W3M = 'w3m'                 # text generation.
-LYNX = 'lynx'               # text generation (if no w3m).
+W3M = 'w3m'                 # primary text file generator.
+LYNX = 'lynx'               # alternate text file generator.
 XMLLINT = 'xmllint'         # Set to '' to disable.
 EPUBCHECK = 'epubcheck'     # Set to '' to disable.
 # External executable default options.
@@ -54,8 +54,8 @@ ASCIIDOC_OPTS = ''
 BACKEND_OPTS = ''
 DBLATEX_OPTS = ''
 FOP_OPTS = ''
-LYNX_OPTS = ''
-W3M_OPTS = '-cols 70 -T text/html -no-graph'
+LYNX_OPTS = '-dump'
+W3M_OPTS = '-dump -cols 70 -T text/html -no-graph'
 XSLTPROC_OPTS = ''
 
 ######################################################################
@@ -821,7 +821,7 @@ class A2X(AttrDict):
             shell('"%s" %s --conf-file "%s" -b html4 -a "a2x-format=%s" -o "%s" "%s"' %
                  (self.asciidoc, self.asciidoc_opts, self.asciidoc_conf_file('text.conf'),
                   self.format, html_file, self.asciidoc_file))
-            cmd = '"%s" -dump %s "%s" > "%s"' % (LYNX, LYNX_OPTS, html_file, text_file)
+            cmd = '"%s" %s "%s" > "%s"' % (LYNX, LYNX_OPTS, html_file, text_file)
             shell(cmd)
         else:
             # Use w3m(1).
@@ -830,7 +830,7 @@ class A2X(AttrDict):
             opts = '%s --output "%s"' % (self.xsltproc_opts, html_file)
             exec_xsltproc(self.xsl_stylesheet(), docbook_file,
                     self.destination_dir, opts)
-            cmd = '"%s" -dump %s "%s" > "%s"' % (W3M, W3M_OPTS, html_file, text_file)
+            cmd = '"%s" %s "%s" > "%s"' % (W3M, W3M_OPTS, html_file, text_file)
             shell(cmd)
         if not self.keep_artifacts:
             shell_rm(html_file)

7da86e268f28945301dc77839f13e6a72dda56e6
Dan Allen <dan.j.allen@gmail.com>
2015-05-13 16:49:31 -0600

    make delimiters around LaTeX math optional
    Remove the delimiters around LaTeX math equations when capturing the
    user's input. Then, add the delimiters around LaTeX math equations in
    the generated output.
    
    This change makes the behavior of the latexmath block and macros
    consistent with the asciimath block and macros. Authors can now
    optionally write LaTeX math equations without delimiters in the same way
    that AsciiMath is written.
    
    This change _is_ backwards compatible with existing documents.

---
+++ filters/unwraplatex.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+'''
+NAME
+    unwraplatex - Removes delimiters from LaTeX source text
+
+SYNOPSIS
+    latex2img STDIN
+
+DESCRIPTION
+    This filter reads LaTeX source text from STDIN and removes the
+    surrounding \[ and \] delimiters.
+'''
+
+import re, sys
+
+sys.stdout.write(re.sub("(?s)\A(?:\\\\\[\s*)?(.*?)(?:\\\\\])?\Z", "\\1", sys.stdin.read().rstrip()))
+# NOTE append endline in result to prevent 'no output from filter' warning
+sys.stdout.write("\n")
